1a078f91853b6ea815a0663ad12f2054aa0f7e48
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-30 20:11:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,11 @@
 def turn(board)
   puts "please enter 1-9"
 end
+def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+      return true
+   end
+end
 
 def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
