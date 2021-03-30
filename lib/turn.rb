775e3560f03359a6ce6d8f2c776707557e484ec0
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-30 20:45:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,13 +28,12 @@ def valid_move?(board, index)
    end
 end
 
-
 def turn(board)
    puts "Please enter 1-9:"
    user_input = gets.strip
    index = input_to_index(user_input)
    if valid_move?(board, index)
-      move(board, index, user_input(board))
+      move(board, index, (board))
       turn(board)
    end
    display_board(board)
