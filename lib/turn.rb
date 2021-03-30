dc5949297b7d92b3dc1cf57480cf4e2167ca48bf
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-30 20:36:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,3 +31,29 @@ def valid_move?(board, index)
       return true
    end
 end
+
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+
+def turn(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
+   end
+   display_board(board)
+end
