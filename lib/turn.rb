ac77feecee68395b44bbf037299469cf9fae943b
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-30 20:47:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+
 def move(array, index, value= "X")
   array[index] = value
 end
@@ -28,13 +29,14 @@ def valid_move?(board, index)
    end
 end
 
+
 def turn(board)
    puts "Please enter 1-9:"
    user_input = gets.strip
    index = input_to_index(user_input)
    if valid_move?(board, index)
-      move(board)
-      turn(board, index)
+      move(board, index, current_player(board))
+      turn(board)
    end
    display_board(board)
 end
